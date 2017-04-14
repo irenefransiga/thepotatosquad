73e92f29bf68077bfcc8aefb2fe560f11f693bdb
Irene Fransiga <if30@uw.edu>
2017-04-13 23:26:33 -0700

    Divided the main method to more smaller methods added java doc for each method

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,72 +5,70 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-
-import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 /**
- * 
  * @author Scott Hiraki, Irene Fransiga, Mamadou Barry, Tanvir Singh, David Mkrtychyan
  * @version 0.1
  * This program makes a simple GUI that has a Menu item "About..." that when clicked, opens a new window and displays
  * our team name, group members, email, and website.
  */
 public class Main {
-    
+	/** myFrame. **/
+    private static JFrame myFrame = new JFrame();
     /**
      * Main to start off the program.
      * @param args
      */
     public static void main(String[] args) {
         
-        //new Main();
-        JFrame guiFrame = new JFrame();
-        
         //make sure the program exits when the frame closes
-        guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        guiFrame.setTitle("The Potato Squad");
-        guiFrame.setSize(650, 650);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setTitle("The Potato Squad");
+        myFrame.setSize(650, 650);
       
         //This will center the JFrame in the middle of the screen
-        guiFrame.setLocationRelativeTo(null);
-        //create menu bar at the top
-        JMenuBar menuBar = new JMenuBar();
-        ImageIcon icon = new ImageIcon("basket.png");
-        JMenuItem about = new JMenuItem("About...");
-        about.setIcon(icon);
-        menuBar.add(about);
-        //adds menubar to the jframe.
-        guiFrame.add(menuBar, BorderLayout.NORTH);
+        myFrame.setLocationRelativeTo(null);
+        myFrame.setJMenuBar(createMenuBar());
         
-        //The ActionListener class is used to handle the
+        //make sure the JFrame is visible
+        myFrame.setVisible(true);
+    }
+    /**
+     * Create a menubar to be placed as JMenuBar of myFrame.
+     * @return menuBar the menuBar consist of JMenu and/or JMenuItem
+     */
+    public static JMenuBar createMenuBar(){
+    	final JMenuBar menuBar = new JMenuBar();
+    	menuBar.add(createAbout());
+        return menuBar;
+    }
+    /**
+     * Create a menuItem of "About...".
+     * the About will be information about our group.
+     * @return aboutMenu that is basically a button of JMenuItem
+     */
+    public static JMenuItem createAbout(){
+    	final ImageIcon aboutIcon = new ImageIcon("basket.png");
+    	final JMenuItem aboutMenu = new JMenuItem("About...", aboutIcon);
+    	//The ActionListener class is used to handle the
         //event that happens when the user clicks the button.
-        about.addActionListener(new ActionListener()
-        {
+    	aboutMenu.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent event)
-            {
-            	
-            	JOptionPane.showMessageDialog(guiFrame,
+            public void actionPerformed(ActionEvent event){
+            	JOptionPane.showMessageDialog(myFrame,
             		    "Members:"
-            		    + "\nIrene Fransiga"
-            		    + "\nScott Hiraki"
-            		    + "\nDavid Mkrtychyan"
-            		    + "\nMamadou Barry"
-            		    + "\nTanvir Singh(Beni)"
-            		    + "\n\n"
-            		    + "\nEmail:"
-            		    + "\nthepotatosquad05@gmail.com"
-            		    + "\nURL:"
-            		    + "\nhttp://thepotatosquad.tk/",
+            		    + "\nIrene Fransiga\nScott Hiraki"
+            		    + "\nDavid Mkrtychyan\nMamadou Barry"
+            		    + "\nTanvir Singh(Beni)n\n"
+            		    + "\nEmail:\nthepotatosquad05@gmail.com"
+            		    + "\nURL:\nhttp://thepotatosquad.tk/",
             		    "We are 'The Potato Squad'",
             		    JOptionPane.PLAIN_MESSAGE);
             }
         });
-        
-        //make sure the JFrame is visible
-        guiFrame.setVisible(true);
+    	return aboutMenu;
     }
     
 }
